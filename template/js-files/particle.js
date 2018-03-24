3b86c8dd0be5426a9629d2f5d5ceeb4ea8fa434a
LuffyKing <aderinwale17@gmail.com>
2018-03-24 04:22:57 +0100

    Delete particle.js

--- template/js-files/particle.js
+++
@@ -1,5 +0,0 @@
-import path fomr 'path';
-
-particlesJS.load('particles-js', path.resolve(__dirname,'particlesjs-config2.json'), function() {
-  console.log('callback - particles.js config loaded');
-});
