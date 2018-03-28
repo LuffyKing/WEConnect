4eafc555639e2a87bb83dacd252f459945302123
LuffyKing <aderinwale17@gmail.com>
2018-03-28 12:54:22 +0100

    Migrations for postgres database

--- server/migrations/20180318224627-create-user.js
+++ server/migrations/20180318224627-create-user.js
@@ -1,9 +1,44 @@
-import models from '../models/';
-
 module.exports = {
-  up: queryInterface =>
-    queryInterface.createTable(models.User.tableName, models.User.attributes),
-  down: queryInterface =>
-    queryInterface.dropTable(models.User.tableName)
-
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    password: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      validate: {
+        isEmail: true
+      },
+      allowNull: false
+    },
+    mobile: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false
+    },
+    userid: {
+      type: Sequelize.UUID,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+      allowNull: false
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('Users')
 };
