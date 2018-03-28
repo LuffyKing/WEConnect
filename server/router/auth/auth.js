f50d746ac68f4072017890a6899c3e34d55c1220
LuffyKing <aderinwale17@gmail.com>
2018-03-28 12:56:29 +0100

    Added JWT to routers

--- server/router/auth/auth.js
+++ server/router/auth/auth.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import authController from '../../controllers/auth';
-import { users } from '../../dummy-data/database';
 import validation from '../../middlewares/validation';
 /**
  * It initializes an express router
@@ -9,14 +8,13 @@ const authRouter = express.Router();
 /**
  * It initializes an instance auth controller
  */
-const auth = new authController(users);
 /**
  *It creates a post route on /login to work with the loginUser method
  */
-authRouter.post('/login', validation.loginValidator, auth.loginUser);
+authRouter.post('/login', validation.loginValidator, authController.loginUser);
 /**
  *It creates a post route on /signup to work with the signupUser method
  */
-authRouter.post('/signup', validation.signUpValidator, auth.signupUser);
+authRouter.post('/signup', validation.signUpValidator, authController.signupUser);
 
 export default authRouter;
--- server/router/review/review.js
+++ server/router/review/review.js
@@ -1,23 +1,19 @@
 import express from 'express';
 import ReviewsController from '../../controllers/reviews';
-import { reviews, businesses, users } from '../../dummy-data/database';
 import validation from '../../middlewares/validation';
 /**
  * It initializes an express router and uses the merges the route to get the
  * businessid parameter
  */
 const reviewRouter = express.Router({ mergeParams: true });
-/**
- * It initializes an instance reviews controller
- */
-const reviewsCont = new ReviewsController(businesses, reviews, users);
+
 /**
  *It creates a post route on / to work with the addReview method
  */
-reviewRouter.post('/', validation.addReviewValidator, reviewsCont.addReview);
+reviewRouter.post('/', validation.addReviewValidator, ReviewsController.addReview);
 /**
  *It creates a get route on / to work with the addReview method
  */
-reviewRouter.get('/', validation.businessidValidator, reviewsCont.getAllReviews);
+reviewRouter.get('/', validation.businessidValidator, ReviewsController.getAllReviews);
 
 export default reviewRouter;
