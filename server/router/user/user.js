d2ab18629d7a210ae6298cf357c707a2bc80a89f
LuffyKing <aderinwale17@gmail.com>
2018-03-29 06:47:30 +0100

    Heroku fixes

--- server/router/user/user.js
+++ server/router/user/user.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import User from '../../controllers/User';
+import User from '../../controllers/Users';
 import validation from '../../middlewares/validation';
 
 const authRouter = express.Router();
