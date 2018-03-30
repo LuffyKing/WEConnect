27e232d8baab8eaf909c6ee0c5fbe92706f47894
LuffyKing <aderinwale17@gmail.com>
2018-03-30 15:54:58 +0100

    Added token verification

--- server/router/review/review.js
+++ server/router/review/review.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import reviews from '../../controllers/reviews2';
 import validation from '../../middlewares/validation';
+import verifyToken from '../../middlewares/verifyToken';
 
 const reviewRouter = express.Router({ mergeParams: true });
 
-reviewRouter.post('/', validation.addReviewValidator, reviews.addReview);
+reviewRouter.post('/', verifyToken, validation.addReviewValidator, reviews.addReview);
 
-reviewRouter.get('/', validation.businessidValidator, reviews.getAllReviews);
+reviewRouter.get('/', verifyToken, validation.businessidValidator, reviews.getAllReviews);
 
 export default reviewRouter;
